2f46cd9e136ac011c0745789948d5227d1a687f4
hozlucas28 <hozlucas28@gmail.com>
2023-01-21 23:41:21 -0300

    Upload lesson

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,43 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/* --------------------------------------------------------------------------
+ * APUNTES:
+ * 		   La configuración presente en este archivo me permite
+ * 		   utilizar 'styled-components' cuando utilizo SSR (Server Side
+ * 		   Rendering).
+ *
+ *
+ * IMPORTANTE:
+ *  			  Esta configuración va acompañada del archivo: <.babelrc>, y
+ * 				  sus dependencias correspondientes.
+-------------------------------------------------------------------------- */
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document from 'next/document';
+
+import { ServerStyleSheet } from 'styled-components';
+
+export default class myDocument extends Document {
+	static async getInitialProps(ctx) {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanced: (App) => (props) => sheet.collectStyles(<App {...props} />)
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				)
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
 }
